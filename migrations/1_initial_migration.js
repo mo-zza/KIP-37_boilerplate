067b546dd35ce8d5b2224545891c8c512a97714a
mo-zza <bigsmalljoe@gmail.com>
2022-01-01 16:24:13 +0900

    feat: Initial commit

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,7 @@
+const Migrations = artifacts.require("Migrations");
+const KlaytnGreeter = artifacts.require("KIP37Token")
+
+module.exports = function (deployer) {
+  deployer.deploy(Migrations);
+  deployer.deploy(KlaytnGreeter, 'testUri.com')
+};
---
+++ truffle-config.example.js
@@ -0,0 +1,30 @@
+const HDWalletProvider = require("truffle-hdwallet-provider-klaytn");
+
+const privateKey = "" // Enter your private key;
+
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    testnet: {
+      provider: () => new HDWalletProvider(privateKey, "https://your.baobab.en.url:8651"),
+      network_id: '1001', //Klaytn baobab testnet's network id
+      gas: '8500000',
+      gasPrice: null
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(privateKey, "https://your.cypress.en.url:8651"),
+      network_id: '8217', //Klaytn mainnet's network id
+      gas: '8500000',
+      gasPrice: null
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.5.0"
+    }
+  }
+};
